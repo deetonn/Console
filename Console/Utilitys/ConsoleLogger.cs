ea6d75aa47bce27bf35fe636d0e83cdddf1cac27
Deeton Rushton <deetonrushton1@outlook.com>
2022-11-04 15:22:42 +0000

    Basic ConsoleLogger
    This just logs text to the console, displaying what object sent the message and the message contents

---
+++ Console/Utilitys/ConsoleLogger.cs
@@ -0,0 +1,15 @@
+﻿namespace Console.Utilitys;
+
+public class ConsoleLogger : ILogger
+{
+    public void Info(object sender, string message)
+    {
+        System.Console.WriteLine($"({sender.GetType().Name}) [INFO] {message}");
+    }
+
+    public void Err(object sender, string message)
+    {
+        System.Console.WriteLine($"({sender.GetType().Name}) [ERROR] {message}");
+
+    }
+}
