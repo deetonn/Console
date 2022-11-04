8e560bddea0f3453f454427113bd3150ea801467
Deeton Rushton <deetonrushton1@outlook.com>
2022-11-04 15:18:08 +0000

    Add a message to the UI tray
    The UI tray is mainly useless for a real terminal application.
    
    But for a GUI application, it would essentially be a notification tray.

---
+++ Console/Commands/Builtins/Tray/AddTrayCommand.cs
@@ -0,0 +1,25 @@
+﻿namespace Console.Commands.Builtins;
+
+public class AddTrayCommand : BaseBuiltinCommand
+{
+    public override string Name => "AddTray";
+    public override string Description => "Add a queued message";
+    public override DateTime? LastRunTime { get; set; } = null;
+    public override int Run(List<string> args, Terminal parent)
+    {
+        base.Run(args, parent);
+
+        if (args.Count < 1)
+        {
+            // invalid arguments
+            WriteLine("usage: AddTray <message>");
+            return -1;
+        }
+
+        var message = string.Join(' ', args);
+        parent.Ui.Tray.AddMessage(message);
+        parent.Ui.SetTitle("Added 1 new message!");
+        
+        return 0;
+    }
+}
