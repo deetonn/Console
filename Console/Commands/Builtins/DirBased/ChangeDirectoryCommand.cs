472249490892a7b6388dd5d54d4546a76371a3a5
Deeton Rushton <deetonrushton1@outlook.com>
2022-11-04 15:21:14 +0000

    CD Command
    Provides a way to change the current working directory.
    
    Such as `cd ../../.` or `cd C:/%UserProfile%/Projects`

---
+++ Console/Commands/Builtins/DirBased/ChangeDirectoryCommand.cs
@@ -0,0 +1,52 @@
+﻿using System.Security;
+
+namespace Console.Commands.Builtins;
+
+public class ChangeDirectoryCommand : BaseBuiltinCommand
+{
+    public override string Name => "Cd";
+    public override string Description => "Change the active directory";
+    public override DateTime? LastRunTime { get; set; } = null;
+    public override int Run(List<string> args, Terminal parent)
+    {
+        base.Run(args, parent);
+
+        if (args.Count != 1)
+        {
+            return CommandReturnValues.BadArguments;
+        }
+
+        var path = args.First();
+
+        if (string.IsNullOrEmpty(path))
+        {
+            WriteLine("Cannot set the working directory to an empty string.");
+            return CommandReturnValues.BadArguments;
+        }
+
+        try
+        {
+            Environment.CurrentDirectory = path;
+        }
+        catch (DirectoryNotFoundException ex)
+        {
+            WriteLine($"{ex.Message}");
+            return CommandReturnValues.BadArguments;
+        }
+        catch (SecurityException ex)
+        {
+            WriteLine($"{ex.Message}");
+            return -1;
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            // same as above, for some reason SecurityException is not thrown?
+            WriteLine($"{ex.Message}");
+            return -1;
+        }
+        
+        parent.WorkingDirectory = Environment.CurrentDirectory;
+        
+        return 1;
+    }
+}
