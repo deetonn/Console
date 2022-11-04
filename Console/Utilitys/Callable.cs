35a89fa7b930e3f98646d1cb68ca031a20418379
Deeton Rushton <deetonrushton1@outlook.com>
2022-11-04 15:20:14 +0000

    Add mess about type
    Was theory testing myself to see if a true generic callable could be a thing.

---
+++ Console/Utilitys/Callable.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.Remoting;
+
+namespace Console.Utilitys;
+
+public class Callable
+{
+    private dynamic Invokable;
+    
+    public Callable(dynamic callable)
+    {
+        var type = (Type)callable.GetType();
+
+        if (type is null)
+        {
+            throw new ArgumentException("failed to get type information of callable");
+        }
+
+        var methods = type.GetMethods();
+        var hasCall = methods.Any(x => x.Name == "Invoke");
+
+        if (!hasCall)
+        {
+            throw new ArgumentException("argument is not callable. Must have the `Invoke` method.");
+        }
+
+        Invokable = callable;
+    }
+
+    public dynamic Function
+        => Invokable;
+}
