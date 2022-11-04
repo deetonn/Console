b27c04c4ab093d43446739d75c0e89e0c2e82c2a
Deeton Rushton <deetonrushton1@outlook.com>
2022-11-04 15:26:51 +0000

    The base for any user interface
    Anything that implements this must do UI specific tasks that will produce the effect of what the name (function name) describes.
    
    Any where you want to display output to the user, must use the active IUserInterface instance.
    
    This is stored in Terminal, which is passed everywhere.

---
+++ Console/UserInterface/IUserInterface.cs
@@ -0,0 +1,50 @@
+﻿using Console.UserInterface.UiTypes;
+
+namespace Console.UserInterface;
+
+/// <summary>
+/// The base for a user interface.
+/// </summary>
+public interface IUserInterface
+{
+    /// <summary>
+    /// Display a message on the user interface
+    /// </summary>
+    /// <param name="message">The message to display</param>
+    /// <param name="type">The type of message the caller is sending</param>
+    public void DisplayLine(string message, Severity type = Severity.None);
+    
+    /// <summary>
+    /// Display a message on the user interface
+    /// </summary>
+    /// <param name="message">The message to display</param>
+    /// <param name="type">The type of message the caller is sending</param>
+    public void Display(string message, Severity type = Severity.None);
+
+    public void DisplayLinePure(string message);
+    public void DisplayPure(string message);
+    
+    /// <summary>
+    /// Set the user interface's main title.
+    /// </summary>
+    /// <param name="message">The new title</param>
+    public void SetTitle(string message);
+    
+    /// <summary>
+    /// Clear the active display.
+    /// </summary>
+    public void Clear();
+    
+    /// <summary>
+    /// Read a line from the interfaces stdin.
+    /// </summary>
+    /// <returns>The line that has been read, after [Enter] has been pressed.</returns>
+    public string GetLine();
+    
+    /// <summary>
+    /// The applications message tray. This should be used to display
+    /// messages that aren't relevant in the moment, but can be
+    /// useful.
+    /// </summary>
+    public IMessageTray Tray { get; }
+}
