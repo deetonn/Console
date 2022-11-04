6e522c56a534a37cbe80a128f56a3dfbb32ec1cb
Deeton Rushton <deetonrushton1@outlook.com>
2022-11-04 15:31:05 +0000

    Arguments for QueueCommand

---
+++ Console/Commands/Builtins/Arguments/QueueCommandArguments.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+
+namespace Console.Commands.Builtins;
+
+internal class QueueCommandArguments
+{
+    [Option('n', "name")] 
+    public string CommandName { get; set; } = null!;
+    
+    [Option('b', "begin")]
+    public bool WantsQueue { get; set; }
+    
+    [Option('d', "dequeue")]
+    public bool WantsDequeue { get; set; }
+    
+    [Option('a', "arguments")]
+    public IEnumerable<string>? DequeueArgs { get; set; }
+}
