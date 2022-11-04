58c3b6002149f2ddc3472e9a03d415bd74e20866
Deeton Rushton <deetonrushton1@outlook.com>
2022-11-04 15:33:49 +0000

    Helper class
    Initialize a user interface using a simple function.

---
+++ Console/UserInterface/Ui.cs
@@ -0,0 +1,16 @@
+﻿using Console.UserInterface.UiTypes;
+
+namespace Console.UserInterface;
+
+public static class Ui
+{
+    public static IUserInterface Create(UiType type)
+    {
+        return type switch
+        {
+            UiType.Console => new NativeConsoleUi(),
+            UiType.ImGui => throw new NotImplementedException("implement ImGui user interface"),
+            _ => throw new NotImplementedException($"implement {nameof(UiType)}.{type}")
+        };
+    }
+}
