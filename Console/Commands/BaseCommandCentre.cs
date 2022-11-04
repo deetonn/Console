f58fbca2cc707a73d0833d9983e9c056fe163c3c
Deeton Rushton <deetonrushton1@outlook.com>
2022-11-04 15:19:46 +0000

    Provides a base for ICommandCentre
    This is just a placeholder at the moment until more specific command centres are needed.

---
+++ Console/Commands/BaseCommandCentre.cs
@@ -0,0 +1,139 @@
+﻿using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using Console.Utilitys;
+
+namespace Console.Commands;
+
+public class BaseCommandCentre : ICommandCentre
+{
+    private const string PathVariableName = "PATH";
+    
+    public BaseCommandCentre()
+    {
+        Elements = LoadBuiltinCommands();
+        ((List<ICommand>)Elements).AddRange(LoadPathExecutables());
+        PausedCommands = new List<ICommand>();
+    }
+    
+    public IList<ICommand> Elements { get; }
+    public IList<ICommand> PausedCommands { get; }
+
+    public int Run(string name, List<string> args, Terminal owner)
+    {
+        return Elements
+                   .FirstOrDefault(x => x.Name.ToLower().Equals(name.ToLower()))?
+                   .Run(args, owner)
+               ?? int.MinValue;
+    }
+
+    public bool CommandExists(string name)
+    {
+        return Elements.Any(x => x.Name == name);
+    }
+
+    public List<ICommand> LoadBuiltinCommands()
+    { 
+        var instances = new List<ICommand>();
+        var types = Assembly.GetExecutingAssembly().GetTypes();
+        var @base = typeof(ICommand);
+
+        var index = 0;
+        for (; index < types.Length; index++)
+        {
+            var type = types[index];
+            if (!type.IsAssignableTo(@base)
+                || type == @base
+                || type == typeof(BaseBuiltinCommand)
+                || type == typeof(PathFileCommand))
+                continue;
+            var instance = Activator.CreateInstance(type);
+            if (instance is null)
+                continue;
+            instances.Add((ICommand)instance);
+        }
+
+        return instances;
+    }
+
+    public List<ICommand> LoadPathExecutables()
+    {
+        var logger = Singleton<ILogger>.Instance();
+
+        var path = Environment.GetEnvironmentVariable(PathVariableName);
+        if (path is null)
+        {
+            return Array.Empty<ICommand>().ToList();
+        }
+
+        var results = new List<ICommand>();
+        var dirs = path.Split(';');
+        
+        foreach (var directory in dirs)
+        {
+            // get the files in that directory,
+            // construct a PathFileCommand with the FileInfo
+
+            string[]? files;
+
+            try
+            {
+                files = Directory.GetFiles(directory, "*.exe");
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                // The PATH directory does not exist.
+                logger.Err(this, $"failed to load path `{directory}` [{ex.Message}]");
+                continue;
+            }
+            catch (IOException ex)
+            {
+                // The PATH directory points to a file
+                logger.Err(this, $"failed to load path `{directory}` [{ex.Message}]");
+                continue;
+            }
+            
+            var commands = files
+                .Select(x => new FileInfo(x));
+
+            results.AddRange(
+                commands
+                    .Select(fileInfo => new PathFileCommand(fileInfo)
+                    )
+                );
+        }
+
+        return results;
+    }
+
+    public int AttemptToQueueCommand(string name, List<string> args, Terminal owner)
+    {
+        if (!CommandExists(name))
+        {
+            return CommandReturnValues.NoSuchCommand;
+        }
+
+        var instance = Elements.First(x => x.Name == name);
+
+        if (instance is not PathFileCommand command)
+        {
+            return CommandReturnValues.CQueueBadType;
+        }
+        
+        var started = command.StartThenPause(args);
+        PausedCommands.Add(instance);
+        
+        return started ? 0 : CommandReturnValues.FailedToStartProcess;
+    }
+
+    public PathFileCommand? FinishQueuedCommand(string command)
+    {
+        if (PausedCommands.Count == 1)
+        {
+            return PausedCommands[0] as PathFileCommand;
+        }
+
+        return PausedCommands
+                .FirstOrDefault(x => x.Name == command)
+            as PathFileCommand;
+    }
+}
