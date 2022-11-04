23c57d04a2ccf7139130553d89ba0494ba426cce
Deeton Rushton <deetonrushton1@outlook.com>
2022-11-04 15:30:49 +0000

    Queue an executable to run when signaled.
    You can start a loaded command (must be a BasePathCommand) and it will be paused until told to resume
    
    Usage(s):
       `queue --name notepad.exe -b` -- starts notepad.exe and paused it.
    
       `queue --name notepad.exe --dequeue` -- will resume the process.

---
+++ Console/Commands/Builtins/Etc/QueueCommand.cs
@@ -0,0 +1,70 @@
+﻿using CommandLine;
+using CommandLine.Core;
+
+namespace Console.Commands.Builtins;
+
+public class QueueCommand : BaseBuiltinCommand
+{
+    public override string Name => "Queue";
+    public override string Description => "Queue a command to run when asked to.";
+    public override DateTime? LastRunTime { get; set; } = null;
+    public override int Run(List<string> args, Terminal parent)
+    {
+        base.Run(args, parent);
+
+        var arguments =
+            Parser.Default
+                .ParseArguments<QueueCommandArguments>(args)
+                .Value;
+
+        if (arguments is null)
+        {
+            return CommandReturnValues.BadArguments;
+        }
+
+        if (arguments.WantsQueue)
+        {
+            var commandArgs = (arguments.DequeueArgs != null)
+                ? arguments.DequeueArgs.ToList()
+                : Array.Empty<string>().ToList();
+            
+            return DoQueue(arguments.CommandName, commandArgs, parent);
+        }
+
+        if (arguments.WantsDequeue)
+        {
+            var commandArgs = (arguments.DequeueArgs != null)
+                ? arguments.DequeueArgs.ToList()
+                : Array.Empty<string>().ToList();
+
+            return DoDequeue(commandArgs, arguments.CommandName, parent);
+        }
+
+        // list all active queued commands.
+       
+        WriteLine($"There is currently {parent.Commands.PausedCommands.Count} queued.\n");
+        foreach (var queuedCommand in parent.Commands.PausedCommands)
+        {
+            WriteLine($"{queuedCommand.Name} -- Queued since {queuedCommand.LastRunTime?.ToShortTimeString()}");
+        }
+
+        return 0;
+    }
+
+    private int DoQueue(string name, List<string> args, Terminal parent)
+    {
+        return parent.Commands.AttemptToQueueCommand(name, args, parent);
+    }
+
+    private int DoDequeue(List<string> args, string name, Terminal parent)
+    {
+        var thing = parent.Commands.FinishQueuedCommand(name);
+
+        if (thing == null)
+        {
+            return CommandReturnValues.CQueueNotFound;
+        }
+        
+        return thing.ResumeExecution(args, parent);
+    }
+}
