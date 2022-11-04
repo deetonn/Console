89a486584340d17fad722b5a39f912d8c0493272
Deeton Rushton <deetonrushton1@outlook.com>
2022-11-04 15:23:16 +0000

    IMessageTray base class
    This class implements and provides an IMessageTray in the most simple form possible.

---
+++ Console/UserInterface/ConsoleMessageTray.cs
@@ -0,0 +1,23 @@
+﻿using Console.UserInterface.UiTypes;
+
+namespace Console.UserInterface;
+
+public class ConsoleMessageTray : IMessageTray
+{
+    public List<string> Messages { get; }
+
+    public ConsoleMessageTray()
+    {
+        Messages = new List<string>();
+    }
+    
+    public void AddMessage(string message)
+    {
+        Messages.Add(message);
+    }
+
+    public void ClearMessages()
+    {
+        Messages.Clear();
+    }
+}
