84535833e72ca25805b2df769620afa833918015
Deeton Rushton <deetonrushton1@outlook.com>
2022-11-04 15:16:27 +0000

    Add About
    This just displays an about message

---
+++ Console/Commands/Builtins/Informational/AboutCommand.cs
@@ -0,0 +1,18 @@
+﻿namespace Console.Commands.Builtins;
+
+public class AboutCommand : BaseBuiltinCommand
+{
+    public override string Name => "About";
+    public override string Description => "About Console";
+    public override DateTime? LastRunTime { get; set; } = null;
+    public override int Run(List<string> args, Terminal parent)
+    {
+        base.Run(args, parent);
+        
+        WriteLine("Terminal is a project, made for fun.");
+        WriteLine("It attempts to emulate Windows cmd.");
+        WriteLine("While also keeping the nice feel of bash alive.");
+
+        return CommandReturnValues.DontShowText;
+    }
+}
