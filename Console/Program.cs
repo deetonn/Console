7e9e0ab59753f5c437c6e1f1fb0844c25abe0ff2
Deeton Rushton <deetonrushton1@outlook.com>
2022-11-04 15:28:51 +0000

    Add Entry Point

---
+++ Console/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Console;
+using Console.UserInterface.UiTypes;
+using Console.Utilitys;
+
+Singleton<ILogger>.InitTo(new ConsoleLogger());
+
+var wantsUi = args.Contains("--gui");
+
+var terminal = new Terminal(
+    wantsUi ? UiType.ImGui : UiType.Console
+);
+
+terminal.MainLoop();
