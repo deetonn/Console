fbd8421ff28398f2bff31b00d99908d7f208318e
Deeton Rushton <deetonrushton1@outlook.com>
2022-11-04 15:25:35 +0000

    The base for any message tray

---
+++ Console/UserInterface/UiTypes/IMessageTray.cs
@@ -0,0 +1,10 @@
+﻿namespace Console.UserInterface.UiTypes;
+
+public interface IMessageTray
+{
+    public List<string> Messages { get; }
+
+    public void AddMessage(string message);
+
+    public void ClearMessages();
+}
