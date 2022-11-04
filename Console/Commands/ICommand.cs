fd26dbef62cb597bde32c9405cdae4d44634027e
Deeton Rushton <deetonrushton1@outlook.com>
2022-11-04 15:25:10 +0000

    The base for any command
    Title says it all

---
+++ Console/Commands/ICommand.cs
@@ -0,0 +1,10 @@
+﻿namespace Console.Commands;
+
+public interface ICommand
+{
+    public string Name { get; }
+    public string Description { get; }
+    public DateTime? LastRunTime { get; set; }
+
+    public int Run(List<string> args, Terminal parent);
+}
