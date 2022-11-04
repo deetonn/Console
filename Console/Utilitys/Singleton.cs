d94ed07ad3b8a08e9d27e62e9ae42116ecb11e29
Deeton Rushton <deetonrushton1@outlook.com>
2022-11-04 15:32:53 +0000

    Add Singleton
    Don't think this is totally thread safe just yet, but it's a nice start.

---
+++ Console/Utilitys/Singleton.cs
@@ -0,0 +1,25 @@
+﻿namespace Console.Utilitys;
+
+public class Singleton<T>
+{
+    private static T? _sInstance;
+    private static readonly object _sLock = new();
+
+    public static T Instance()
+    {
+        lock (_sLock)
+        {
+            if (_sInstance is null)
+            {
+                throw new NullReferenceException("must call InitTo on a singleton.");
+            }
+            return _sInstance;
+        }
+    }
+
+    public static void InitTo(T instance)
+    {
+        lock (_sLock)
+            _sInstance = instance;
+    }
+}
