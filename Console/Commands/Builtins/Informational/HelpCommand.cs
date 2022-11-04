639cc50e159f98582b351582007821b600711583
Deeton Rushton <deetonrushton1@outlook.com>
2022-11-04 15:24:57 +0000

    Add Help
    The help command will display all active commands, including executable commands loaded from PATH.
    
    The option --builtin will display only builtin commands.

---
+++ Console/Commands/Builtins/Informational/HelpCommand.cs
@@ -0,0 +1,31 @@
+﻿namespace Console.Commands.Builtins;
+
+public class HelpCommand : BaseBuiltinCommand
+{
+    public override string Name => "Help";
+    public override string Description => "List all active commands.";
+    public override DateTime? LastRunTime { get; set; } = null;
+    public override int Run(List<string> args, Terminal parent)
+    {
+        base.Run(args, parent);
+        var wantsBuiltin = args.Contains("--builtin");
+
+        if (wantsBuiltin)
+        {
+            var builtins = parent.Commands.Elements.Where(x => x is BaseBuiltinCommand);
+            foreach (var command in builtins)
+            {
+                parent.Ui.DisplayLine($"{command.Name}: {command.Description}");
+            }
+            
+            return 2;
+        }
+        
+        foreach (var command in parent.Commands.Elements)
+        {
+            parent.Ui.DisplayLine($"{command.Name}: {command.Description}");
+        }
+
+        return 0;
+    }
+}
