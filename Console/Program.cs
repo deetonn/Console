2d9e84564d2773608b10868f4d841b4f2cbaadbe
deeton rushy <deetonrushton1@outlook.com>
2023-07-26 00:45:05 +0100

    Remove the useless debug code outputting the command-line arguments.
    Added very-simple support for script execution. This is not permanent and is meant for testing the actual scripting environment before implementing a robust way to process command line arguments.
    
    This needs to be addressed soon as there's no doubt they will start to grow.

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,8 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿
+// Necessary to avoid repetitive code fetching singleton instances.
 global using static Console.Logging.StaticLoggerGlobalUseMe;
 
 using Console;
+using Console.Commands.Scripting;
 using Console.UserInterface.UiTypes;
 using Console.Utilitys;
 
@@ -14,14 +15,29 @@ void Main()
 
     var wantsUi = args.Contains(value: "--gui");
 
-    foreach (var arg in args)
-    {
-        System.Console.WriteLine($"{arg}");
-    }
-
     var terminal = new Terminal(
+        // These are the only two UiTypes.
         type: wantsUi ? UiType.ImGui : UiType.Console
     );
+
+    var doesWantScript = args.Contains(value: "--script");
+    if (doesWantScript)
+    {
+        var scriptFile = Array.Find(args, arg => arg.StartsWith(value: "--script="))?.Split(separator: "=")[1];
+        if (scriptFile == null)
+        {
+            Logger().LogError("Main", "No script file was specified.");
+            return;
+        }
+
+        var context = new ScriptExecutionContext(terminal, scriptFile, (message) =>
+        {
+            terminal.WriteLine($"Failure: {message}");
+            Environment.Exit(0);
+        });
+
+        context.Execute(terminal);
+    }
 
     terminal.MainLoop();
 }
