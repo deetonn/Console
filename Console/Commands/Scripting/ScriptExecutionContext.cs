68a5b362ab53bdf5890a4c2d7a781acace4edd65
deeton rushy <deetonrushton1@outlook.com>
2023-07-26 00:40:00 +0100

    This is the beginning of what will be support for scripting.
    This is a very bare-bones execution context that simply executes commands line-by-line.

---
+++ Console/Commands/Scripting/ScriptExecutionContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console.Commands.Scripting;
+
+public delegate void OnSecErr(string message);
+
+public record class CommandExecutionState(ICommand Command, List<string> Args);
+
+public class ScriptExecutionContext
+{
+    // Stands for Console Script File
+    public const string ScriptExtension = ".csf";
+
+    public List<CommandExecutionState> Commands { get; }
+
+    public ScriptExecutionContext(Terminal parent, string file, OnSecErr? err)
+    {
+        Commands = new List<CommandExecutionState>();
+
+        if (!File.Exists(file))
+        {
+            err?.Invoke("The specified file does not exist.");
+            return;
+        }
+
+        var fileInfo = new FileInfo(file);
+
+        if (fileInfo.Extension != ScriptExtension)
+        {
+            err?.Invoke("The specified file is not a script file.");
+            return;
+        }
+
+        var lines = File.ReadAllLines(file);
+        foreach (var line in lines)
+        {
+            var command = line.Split(' ')[0];
+            var args = line.Split(' ').Skip(1).ToList();
+
+            var cmd = parent.Commands.GetCommand(command);
+            if (cmd == null)
+            {
+                err?.Invoke($"The command `{command}` does not exist.");
+                return;
+            }
+
+            Commands.Add(new CommandExecutionState(cmd, args));
+        }
+    }
+
+    public void Execute(Terminal parent)
+    {
+        foreach (var execState in Commands)
+        {
+            execState.Command.Run(execState.Args, parent);
+        }
+    }
+}
