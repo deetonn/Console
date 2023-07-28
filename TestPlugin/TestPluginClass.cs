e960f30d8ae4d88beb5a06785b76c02e22f83507
deeton rushy <deetonrushton1@outlook.com>
2023-07-28 21:30:38 +0100

    REMOVE: TestPlugin.

--- TestPlugin/TestPluginClass.cs
+++ TestPlugin/TestPluginClass.cs
@@ -40,64 +40,17 @@ public class ExamplePlugin : Plugins.IConsolePlugin
     {
     }
 
-    public void OnSettingChange(Terminal terminal, ISettings settings, string settingName, object newValue)
-    {
-    }
-
-    public void OnUnloaded(Terminal terminal)
-    {
-    }
-
-    public bool OnUserInput(Terminal terminal, string input)
+    public bool OnSettingChange(Terminal terminal, ISettings settings, string settingName, object newValue)
     {
         return true;
     }
-}
-
-public class TestPluginClass : Console.Plugins.IConsolePlugin
-{
-    public string Name => "test plugin";
-
-    public string Description => "This is a test";
-
-    public string Author => "Deeton Lee Rushton";
-
-    public Guid Id { get; set; } = Guid.Empty;
-
-    public void OnCommandExecuted(Terminal terminal, ICommand command)
-    {
-        terminal.WriteLine($"[Test] The command `{command.Name}` has been ran!");
-    }
-
-    public void OnLoaded(Terminal terminal)
-    {
-        terminal.Commands.LoadCustomCommand(new TestCommand());
-    }
-
-    public void OnSettingChange(Terminal terminal, ISettings settings, string settingName, object newValue)
-    {
-        if (settingName.StartsWith("test."))
-        {
-            terminal.WriteLine($"You edited the test option `{settingName}`");
-        }
-
-        return;
-    }
 
     public void OnUnloaded(Terminal terminal)
     {
-        terminal.WriteLine("[test] I am going now!");
     }
 
     public bool OnUserInput(Terminal terminal, string input)
     {
-        if (input == "test")
-        {
-            terminal.Ui.Clear();
-            terminal.WriteLine("You cannot say that! [test]");
-            return false;
-        }
-
         return true;
     }
 }
