bf4a2b7e979d4c7ee72e7f3269f323040beaa61e
deeton rushy <deetonrushton1@outlook.com>
2023-08-15 16:15:33 +0100

    Addition+: `echo` command.
    This command can output all arguments. It also accepts markdown, implemented by Spectre.Console.

---
+++ Console/Commands/Builtins/System/EchoCommand.cs
@@ -0,0 +1,20 @@
+﻿namespace Console.Commands.Builtins.System;
+
+public class EchoCommand : BaseBuiltinCommand
+{
+    public override string Name => "echo";
+    public override string Description => "Prints the specified text to the console. (supports markdown using AnsiConsole)";
+    public override int Run(List<string> args, IConsole parent)
+    {
+        var joined = string.Join(" ", args);
+        try
+        {
+            parent.Ui.DisplayLineMarkup(joined);
+        }
+        catch (Exception e)
+        {
+            WriteLine($"failed due to markdown error. ({e.Message})");
+        }
+        return CommandReturnValues.DontShowText;
+    }
+}
