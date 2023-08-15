19952231b8d817eda8d72a06b6d83f97b1eadd8c
deeton rushy <deetonrushton1@outlook.com>
2023-08-15 16:16:02 +0100

    Addition+: `quit` command.
    This command just notifies the mainloop to exit. This command does a proper cleanup.

---
+++ Console/Commands/Builtins/System/QuitCommand.cs
@@ -0,0 +1,18 @@
+﻿namespace Console.Commands.Builtins.System;
+
+internal class QuitCommand : BaseBuiltinCommand
+{
+    public override string Name => "quit";
+
+    public override string Description => "Exit the application with proper cleanup.";
+
+    public override int Run(List<string> args, IConsole parent)
+    {
+        return CommandReturnValues.SafeExit;
+    }
+
+    public override string DocString => $@"
+This command will cause the application to exit.
+This exit operation will cleanup all resources and perform a graceful shutdown.
+";
+}
