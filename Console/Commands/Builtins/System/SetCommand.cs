a77dd0c59820f0c693b6ece66d8bd73f12fbe5e4
deeton rushy <deetonrushton1@outlook.com>
2023-08-20 02:34:53 +0100

    SetCommand: Set environment variables in the current environment
    This command works on a per-session basis, no system environment variables are edited, just the ones for the current instance of Console.
    
    set <env-var-name> <value>

---
+++ Console/Commands/Builtins/System/SetCommand.cs
@@ -0,0 +1,42 @@
+﻿namespace Console.Commands.Builtins.System;
+
+public class SetCommand : BaseBuiltinCommand
+{
+    public override string Name => "set";
+
+    public override string Description => "set an environment variable.";
+
+    public override int Run(List<string> args, IConsole parent)
+    {
+        base.Run(args, parent);
+
+        // syntax: set <env-name> <value>
+
+        if (args.Count < 2)
+        {
+            WriteError($"[[[red]error[/]]] invalid arguments supplied. USAGE: {Name} <env-name> <value>");
+            return -1;
+        }
+
+        var env_variable_name = args[0];
+        var env_variable_new_val = args[1];
+
+        if (Environment.GetEnvironmentVariable(env_variable_name) is null)
+        {
+            WriteError($"no such environment variable: `{env_variable_name}`");
+            return -1;
+        }
+
+        try
+        {
+            Environment.SetEnvironmentVariable(env_variable_name, env_variable_new_val);
+        }
+        catch (Exception e)
+        {
+            WriteError(e.Message);
+            return -1;
+        }
+
+        return 0;
+    }
+}
