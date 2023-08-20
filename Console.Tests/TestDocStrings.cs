2dc590781ca591a822bba06c789bd44581c86e11
deeton rushy <deetonrushton1@outlook.com>
2023-08-20 02:06:36 +0100

    Tests: add test suite to avoid regressions

---
+++ Console.Tests/TestDocStrings.cs
@@ -0,0 +1,219 @@
+using Console.Commands.Builtins;
+using Console.Commands.Builtins.Config;
+using Console.Commands.Builtins.DirBased;
+using Console.Commands.Builtins.Etc;
+using Console.Commands.Builtins.Informational;
+using Console.Commands.Builtins.System;
+using Console.Commands.Builtins.Web;
+using System.Windows.Input;
+
+namespace Console.Tests;
+
+/*
+ * This set of tests check that the `docs` command works as expected for all commands.
+ * It also verifies that all commands have implemented a docstring.
+ * It also makes sure that any doc strings are not using invalid syntax for markup.
+ * If there is any invalid syntax, an exception will be thrown and the test will fail.
+ * this lets us catch these things early.
+*/
+
+[TestClass]
+public class TestDocStrings
+{
+    private IConsole _instance = DummyConsole.Get;
+
+    private void RunTestsFor(Commands.ICommand command)
+    {
+        Assert.AreNotEqual(string.Empty, command.DocString);
+        Assert.AreEqual(0, _instance.Commands.ExecuteFrom(_instance, new ViewDocCommand().Name, command.Name));
+    }
+
+    // Builtins/Config
+
+    [TestMethod]
+    public void TestEditOptionsCommand()
+    {
+        RunTestsFor(new EditOptionCommand());
+    }
+
+    [TestMethod]
+    public void TestListPluginsCommand()
+    {
+        RunTestsFor(new ListPluginsCommand());
+    }
+
+    [TestMethod]
+    public void TestLoadPluginsCommand()
+    {
+        RunTestsFor(new LoadPluginCommand());
+    }
+
+    [TestMethod]
+    public void TestReloadConfigCommand()
+    {
+        RunTestsFor(new ReloadConfigCommand());
+    }
+
+    [TestMethod]
+    public void TestRemoveOptionCommand()
+    {
+        RunTestsFor(new RemoveOptionCommand());
+    }
+
+    [TestMethod]
+    public void TestUnloadPluginCommand()
+    {
+        RunTestsFor(new UnloadPluginCommand());
+    }
+
+    [TestMethod]
+    public void TestViewOptionsCommand()
+    {
+        RunTestsFor(new ViewOptionsCommand());
+    }
+
+    // Builtins/DirBased
+
+    [TestMethod]
+    public void TestCdCommand()
+    {
+        RunTestsFor(new ChangeDirectoryCommand());
+    }
+
+    [TestMethod]
+    public void TestCopyCommand()
+    {
+        RunTestsFor(new CopyCommand());
+    }
+
+    [TestMethod]
+    public void TestDirCommand()
+    {
+        RunTestsFor(new DirCommand());
+    }
+
+    [TestMethod]
+    public void TestLineCountCommand()
+    {
+        RunTestsFor(new LineCountCommand());
+    }
+
+    [TestMethod]
+    public void TestRmDirCommand()
+    {
+        RunTestsFor(new RmDirCommand());
+    }
+
+    [TestMethod]
+    public void TestTouchCommand()
+    {
+        RunTestsFor(new TouchCommand());
+    }
+
+    // Builtins/Etc
+
+    [TestMethod]
+    public void TestAliasCommand()
+    {
+        RunTestsFor(new AliasCommand());
+    }
+
+    [TestMethod]
+    public void TestClearCommand()
+    {
+        RunTestsFor(new ClearBufferCommand());
+    }
+
+    [TestMethod]
+    public void TestEnvCommand()
+    {
+        RunTestsFor(new EnvCommand());
+    }
+
+    [TestMethod]
+    public void TestGenerateCommand()
+    {
+        RunTestsFor(new GenerateCommand());
+    }
+
+    [TestMethod]
+    public void TestQueueCommand()
+    {
+        RunTestsFor(new QueueCommand());
+    }
+
+    [TestMethod]
+    public void TestVwfCommand()
+    {
+        RunTestsFor(new ViewFileCommand());
+    }
+
+    [TestMethod]
+    public void TestViewManyFilesCommand()
+    {
+        RunTestsFor(new ViewManyFilesCommand());
+    }
+
+    // Builtins/Informational
+
+    [TestMethod]
+    public void TestAboutCommand() // useless as fuck
+    {
+        RunTestsFor(new AboutCommand());
+    }
+
+    [TestMethod]
+    public void TestHelpCommand()
+    {
+        RunTestsFor(new HelpCommand());
+    }
+
+    [TestMethod]
+    public void TestLastRanAtCommand()
+    {
+        RunTestsFor(new LastRanAtCommand());
+    }
+
+    [TestMethod]
+    public void TestDocCommand()
+    {
+        RunTestsFor(new ViewDocCommand());
+    }
+
+    // Builtins/System
+
+    [TestMethod]
+    public void TestEchoCommand()
+    {
+        RunTestsFor(new EchoCommand());
+    }
+
+    // SKIPPED: ExitCommand -- it is internal
+
+    [TestMethod]
+    public void TestRunCommand()
+    {
+        RunTestsFor(new RunCommand());
+    }
+
+    [TestMethod]
+    public void TestTaskListCommand()
+    {
+        RunTestsFor(new TaskListCommand());
+    }
+
+    // SKIPPED: tray commands -- they are shit
+
+    // Builtins/Web
+    [TestMethod]
+    public void TestPkgInstallCommand()
+    {
+        RunTestsFor(new PkgInstall());
+    }
+
+    [TestMethod]
+    public void TestsPkgListCommand()
+    {
+        RunTestsFor(new PkgList());
+    }
+}
---
+++ Console.Tests/Utilitys.cs
@@ -0,0 +1,124 @@
+﻿
+using Console.Commands;
+using Console.Commands.Builtins.Web.WebServer;
+using Console.Events;
+using Console.Plugins;
+using Console.UserInterface;
+using Console.UserInterface.Input;
+using Console.UserInterface.UiTypes;
+using Console.Utilitys;
+using Console.Utilitys.Configuration;
+using Console.Utilitys.Options;
+
+using SystemConsole = System.Console;
+
+namespace Console.Tests;
+
+public class DummyUserInterface : IUserInterface
+{
+    public IMessageTray Tray => throw new NotImplementedException();
+
+    public void Clear()
+    {
+        SystemConsole.Clear();
+    }
+
+    public void Display(string message, Severity type = Severity.None)
+    {
+        SystemConsole.Write(message);
+    }
+
+    public void DisplayLine(string message, Severity type = Severity.None)
+    {
+        SystemConsole.WriteLine(message);
+    }
+
+    public void DisplayLineMarkup(string markup)
+    {
+        // This test suite does not care about the markup, just display it.
+        DisplayLine(markup);
+    }
+
+    public void DisplayLinePure(string message)
+    {
+        DisplayLine(message);
+    }
+
+    public void DisplayMarkup(string markup)
+    {
+        Display(markup);
+    }
+
+    public void DisplayPure(string message)
+    {
+        Display(message);
+    }
+
+    public ConsoleKeyInfo GetKey()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetLine(string prompt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetTitle(string message)
+    {
+        SystemConsole.Title = message;
+    }
+}
+
+public class NonfunctionalTerminal : IConsole
+{
+    public string WorkingDirectory { get; set; } = Directory.GetCurrentDirectory();
+
+    public string UnixStyleWorkingDirectory => throw new NotImplementedException();
+
+    public string WdUmDisplay => throw new NotImplementedException();
+
+    public IUserInterface Ui { get; } = new DummyUserInterface();
+
+    public ICommandCentre Commands { get; } = new BaseCommandCentre();
+
+    public ISettings Settings { get; set; }
+
+    public IPluginManager PluginManager { get; }
+
+    // Ignore, not a main part of this project.
+    public IServer? Server { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    // Unused
+    public IInputHandler InputHandler => throw new NotImplementedException();
+
+    public IConfiguration Config { get; } = new Configuration();
+
+    public IEventHandler EventHandler { get; } = new GlobalEventHandler();
+
+    public string GetConfigPath()
+    {
+        // For tests, the config path should be contained.
+        // Using the real, one could pollute developers configuration.
+        return Directory.GetCurrentDirectory();
+    }
+
+    public NonfunctionalTerminal()
+    {
+        Settings = new ConsoleOptions(Directory.GetCurrentDirectory(), this)
+        {
+            IsTestMode = true
+        };
+        PluginManager = new PluginManager(this);
+    }
+}
+
+public static class DummyConsole
+{
+    public static IConsole Get => new NonfunctionalTerminal();
+
+    static DummyConsole()
+    {
+        Singleton<ILogger>.InitTo(new ConsoleLogger());
+    }
+}
