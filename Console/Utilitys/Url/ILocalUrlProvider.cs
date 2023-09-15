bbe81e2ffbe2eee5403f4e781a8b020455e3365a
deeton rushy <deetonrushton1@outlook.com>
2023-09-15 22:32:22 +0100

    Begin work on a way to support local URLs for executing commands.

---
+++ Console/Utilitys/Url/ILocalUrlProvider.cs
@@ -0,0 +1,49 @@
+﻿
+using System.IO.Pipes;
+
+namespace Console.Utilitys.Url;
+
+public record class ExecutionRequest(string Command, List<string> Arguments);
+
+public enum ActionType
+{
+    /// <summary>
+    /// If the data is of this type, this is an execute request.
+    /// The data should be casted to <see cref="ExecutionRequest"/>
+    /// </summary>
+    Execute,
+}
+
+public delegate void OnPipeDataReceived(ActionType action, object obj);
+
+public interface ILocalUrlProvider
+{
+    /// <summary>
+    /// The pipe that is talking to the node process. This process is responsible
+    /// for actually running the web server.
+    /// </summary>
+    public NamedPipeClientStream Pipe { get; }
+
+    /// <summary>
+    /// The base URL to make requests to. This contains the local ip address,
+    /// (localhost) and the port. Example: http://127.0.0.1:0000
+    /// </summary>
+    public string BaseUrl { get; }
+
+    /// <summary>
+    /// Is node installed on the local machine.
+    /// </summary>
+    public bool HasNodeInstalled { get; }
+
+    /// <summary>
+    /// This function will be called whenever the client receives information
+    /// from the server.
+    /// </summary>
+    public event OnPipeDataReceived? OnPipeDataReceived;
+
+    /// <summary>
+    /// Disconnect from the named pipe. 
+    /// </summary>
+    /// <param name="reason">The reason, or null if there isn't one.</param>
+    public void Disconnect(string? reason = null);
+}
---
+++ Console/Utilitys/Url/LocalUrlProvider.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console.Utilitys.Url;
+
+public class LocalUrlProvider : ILocalUrlProvider
+{
+    public string BaseUrl { get; internal set; }
+
+    public bool HasNodeInstalled { get; } = false;
+
+    public NamedPipeClientStream Pipe { get; private set; }
+
+    public event OnPipeDataReceived? OnPipeDataReceived;
+
+    public LocalUrlProvider(IConsole parent)
+    {
+        // firstly check if node is even installed.
+        var (isNodeInstalled, nodePath) = GetNodePath();
+
+        if (!isNodeInstalled)
+        {
+            parent.Commands.ExecuteFrom(parent, "pkg-install", "nodejs", "--prompt");
+        }
+    }
+
+    public void Disconnect(string? reason = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    private (bool NodeExists, string? NodePath) GetNodePath()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            return GetNodePathMingw();
+        }
+
+        throw new NotImplementedException("implement GetNodePath() for gnu based systems.");
+    }
+
+    private static (bool, string?) GetNodePathMingw()
+    {
+        var data = (string?)Registry.GetValue(@"Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Node.js", "InstallPath", null);
+        if (data == null)
+        {
+            return (false, null);
+        }
+        return (true, data);
+    }
+}
