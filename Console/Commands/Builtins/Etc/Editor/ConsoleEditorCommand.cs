607298ebc8e9c320876d4eace59c1a8161a8df68
deeton rushy <deetonrushton1@outlook.com>
2023-09-15 22:29:06 +0100

    Ced: use Dezi for the builtin editor.

---
+++ Console/Commands/Builtins/Etc/Editor/ConsoleEditorCommand.cs
@@ -0,0 +1,39 @@
+﻿
+using dezi;
+using dezi.UiElements;
+
+namespace Console.Commands.Builtins.Etc.Editor;
+
+public class ConsoleEditorCommand : BaseBuiltinCommand
+{
+    public override string Name => "ced";
+
+    public override string Description => "Edit a file.";
+
+    public override int Run(List<string> args, IConsole parent)
+    {
+        base.Run(args, parent);
+
+        if (args.Count != 1)
+        {
+            WriteLine("no file path specified.");
+            return -1;
+        }
+
+        var path = Path.IsPathRooted(args[0])
+            ? args[0] 
+            : Path.Combine(parent.WorkingDirectory, args[0]);
+
+        FileInfo fileInfo;
+        if (!(fileInfo = new FileInfo(path)).Exists)
+        {
+            WriteLine($"could not find file \"{fileInfo.FullName}\".");
+            return -1;
+        }
+
+        var editor = new Ui(new List<string> { fileInfo.FullName });
+        editor.Run();
+
+        return 0;
+    }
+}
