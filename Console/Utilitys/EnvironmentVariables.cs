ec277219b2cbf15fa8dfa8b8fed239c87128fee9
deeton rushy <deetonrushton1@outlook.com>
2023-09-22 06:00:59 +0100

    EnvironmentVariables: Environment Variables class
    Instead of using the native Environment.XXX, this class encapsulates environment variables. This allows us to make custom ones without polluting the actually process environment variables.
    
    This also means any command can access them.

---
+++ Console/Utilitys/EnvironmentVariables.cs
@@ -0,0 +1,79 @@
+﻿
+using Console.Commands.Builtins.Config;
+using Console.Extensions;
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Console.Utilitys;
+
+public class EnvironmentVariables : IEnvironmentVariables
+{
+    public Dictionary<string, string> Variables { get; }
+
+    public EnvironmentVariables()
+    {
+        Variables = new Dictionary<string, string>();
+        Clear();
+    }
+
+    public string? Get(string identifier)
+    {
+        if (!Variables.TryGetValue(identifier, out var value))
+            return null;
+        return value;
+    }
+
+    public bool TryGet(string identifier, [NotNullWhen(true)] out string? value)
+    {
+        value = Get(identifier);
+        return identifier != null;
+    }
+
+    public void Set(string identifier, string key)
+    {
+        Variables[identifier] = key;
+    }
+
+    public void Clear()
+    {
+        Variables.Clear();
+        var variables = (Hashtable)Environment.GetEnvironmentVariables();
+        foreach (DictionaryEntry pair in variables)
+        {
+            // TODO: make this shit look better, wtf is this api design
+            if (pair.Key is string key)
+            {
+                if (pair.Value is string value)
+                {
+                    Variables.Add(key, value);
+                }
+            }
+        }
+    }
+
+    // NOTE: "_" is the return value.
+    //       "$" is the output value.
+
+    public void EnterCommandContext()
+    {
+        Set("$", string.Empty);
+        Set("_", "0");
+    }
+
+    public void AppendCommandOutput(string output)
+    {
+        if (Contains("$"))
+        {
+            Variables["$"] += output;
+        }
+        // TODO: handle the case where command context has not been
+        // entered.
+    }
+
+    public void RegisterCommandResult(int result)
+    {
+        Set("_", result.ToString());
+    }
+
+    public bool Contains(string ident) => Variables.ContainsKey(ident);
+}
