17fdfeef0619c9110ea0b8045eb298b37bf03c9f
deeton rushy <deetonrushton1@outlook.com>
2023-09-22 05:59:56 +0100

    Small helper for dictionarys

---
+++ Console/Extensions/IDictionaryExtensions.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Console.Extensions;
+
+public static class DictionaryExtensions
+{
+    public static void ForEach<T1, T2>(this Dictionary<T1, T2> dict, Action<T1, T2> func)
+        where T1: notnull
+    {
+        foreach (var (k, v) in dict)
+        {
+            func(k, v);
+        }
+    }
+}
