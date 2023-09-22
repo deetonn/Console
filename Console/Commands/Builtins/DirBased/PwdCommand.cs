044c24d2270fc1d23d9b0502341f2134d3a8b78a
deeton rushy <deetonrushton1@outlook.com>
2023-09-22 05:08:20 +0100

    pwd: addition of this command
    This will print out the current working directory.

---
+++ Console/Commands/Builtins/DirBased/PwdCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console.Commands.Builtins.DirBased;
+
+public class PwdCommand : BaseBuiltinCommand
+{
+    public override string Name => "pwd";
+    public override string Description => "Outputs the current working directory.";
+
+    public override int Run(List<string> args, IConsole parent)
+    {
+        base.Run(args, parent);
+
+        var workingDirectory = parent.WorkingDirectory;
+        parent.Ui.DisplayLinePure(workingDirectory);
+
+        return 0;
+    }
+
+    public override string DocString => $@"
+This will output the current working directory in the context
+of the active terminal instance.
+
+[bold cyan]examples[/]:
+  pwd -- outputs the active working directory.
+";
+}
