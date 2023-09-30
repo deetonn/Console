93e0adb32a255211d4f1d3caba3adfcdd5fa8565
deeton rushy <deetonrushton1@outlook.com>
2023-09-30 14:22:17 +0100

    Formatting: add string formatting.
    The only example right now is repeating a string.
    
    Example:
    ```
    cd {:../:x5}
    ```
    
    This will cook the string "../../../../../" into the command before executing. The "x" specifier will repeat the input by whatever number follows it.

---
+++ Console/Formatting/ITextFormatter.cs
@@ -0,0 +1,58 @@
+﻿
+using Console.Errors;
+using System.Diagnostics.CodeAnalysis;
+
+// using TextFormatResult = (string? FormattedValue, CommandError? Error);
+
+namespace Console.Formatting;
+
+public struct TextFormatResult
+{
+    public CommandError? Error;
+    public string? Formatted;
+
+    public static implicit operator TextFormatResult(CommandError error)
+    {
+        return new TextFormatResult
+        {
+            Error = error,
+            Formatted = null
+        };
+    }
+
+    public static implicit operator TextFormatResult(string fmt)
+    {
+        return new TextFormatResult
+        {
+            Error = null,
+            Formatted = fmt
+        };
+    }
+}
+
+public delegate TextFormatResult TextFormatHandler(string fullSpecifier, string userInput, CommandErrorBuilder errBuilder);
+
+public interface ITextFormatter
+{
+    public Dictionary<string, TextFormatHandler> Handlers { get; }
+
+    /// <summary>
+    /// This will add a custom format specifier.
+    /// </summary>
+    /// <param name="handledSpecifier">What does the specifier have to start with to match your format.</param>
+    /// <param name="handler">The function that will handle it.</param>
+    public void AddHandler(string handledSpecifier, TextFormatHandler handler);
+
+    // NOTE: as of now, there is no remove method. It doesn't make sense that they
+    // would be removed?
+
+    /// <summary>
+    /// Attempt to format the <paramref name="text"/> with the <paramref name="specifier"/> 
+    /// and put the result into the out variable <paramref name="result"/>
+    /// </summary>
+    /// <param name="text">The text the user wants to format</param>
+    /// <param name="specifier">The format specifier</param>
+    /// <param name="result">The result</param>
+    /// <returns>true if <paramref name="result"/> contains the formatted string, otherwise false.</returns>
+    public CommandError? Format(string text, string specifier, out string? result);
+}
---
+++ Console/Formatting/TextFormatter.cs
@@ -0,0 +1,70 @@
+﻿
+
+using Console.Errors;
+using Console.Extensions;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Console.Formatting;
+
+public class InlineTextFormatter : ITextFormatter
+{
+    public Dictionary<string, TextFormatHandler> Handlers { get; private set; }
+
+    public InlineTextFormatter()
+    {
+        Handlers = [];
+
+        AddHandler("x", (specifier, input, err) =>
+        {
+            if (specifier.Length == 1)
+            {
+                // This is invalid. There must be a number after it at LEAST.
+                return err.WithMessage($"expected a number to repeat \"{input}\" by.")
+                    .WithNote("example: {:../:x5} would expand to \"../../../../../\".")
+                    .WithNote("it will be repeated [italic]x[/] amount of times...")
+                    .Build();
+            }
+
+            if (!uint.TryParse(specifier[1..], out var count))
+            {
+                return err.WithMessage("invalid number to repeat the sequence by.").Build();
+            }
+
+            return string.Join("", Enumerable.Repeat(input, (int)count));
+        });
+    }
+
+    public void AddHandler(string handledSpecifier, TextFormatHandler handler)
+    {
+        Handlers[handledSpecifier] = handler;
+    }
+
+    public CommandError? Format(string text, string specifier, out string? result)
+    {
+        TextFormatHandler? handler = null;
+
+        Handlers.ForEach((key, value) =>
+        {
+            if (specifier.StartsWith(key))
+            {
+                handler = value;
+            }
+        });
+
+        if (handler is null)
+        {
+            result = null;
+            return new CommandErrorBuilder()
+                .WithMessage($"no handler for the specifier \"{specifier}\"")
+                .WithNote("all handled specifiers below:")
+                .WithNote($"{string.Join(", ", Handlers.Keys)}")
+                .Build();
+        }
+
+        var builder = new CommandErrorBuilder().WithSource($"{{:{text}:{specifier}}}");
+
+        var parseResult = handler.Invoke(specifier, text, builder);
+        result = parseResult.Formatted;
+        return parseResult.Error;
+    }
+}
