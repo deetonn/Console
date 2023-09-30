5b2c692547a199b54c14b1b56f4a3a608a57c518
deeton rushy <deetonrushton1@outlook.com>
2023-09-30 04:46:02 +0100

    PrettyErrors: update this

--- TestPlugin/TestPluginClass.cs
+++ TestPlugin/TestPluginClass.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Transactions;
-using Console.Commands;
-using Console.Utilitys.Options;
+﻿using Console.Commands;
+using Console.Errors;
 
 namespace Console;
 
@@ -10,7 +8,7 @@ public class TestCommand : BaseBuiltinCommand
     public override string Name => "test";
     public override string Description => "This command exists for testing purposes";
 
-    public override int Run(List<string> args, IConsole parent)
+    public override CommandResult Run(List<string> args, IConsole parent)
     {
         base.Run(args, parent);
 
