ce512245c5c091a1ecb10cbfc0d371ac0f865300
deeton rushy <deetonrushton1@outlook.com>
2023-09-30 04:43:29 +0100

    General commit

--- Console/Web/DownloadData.cs
+++ Console/Web/DownloadData.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Console.Web;
+﻿namespace Console.Web;
 
 /// <summary>
 /// The version information about the file.
