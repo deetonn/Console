ea7e41f5cdca8a08d600cf3b3619ee60be9a49d9
deeto <deeto@LOCAL>
2024-01-22 13:01:58 +0000

    ElevateCommand: add a command that automatically elevates the current instance (has to restart)

--- Console/Commands/Builtins/Etc/ClearBuffer.cs
+++ Console/Commands/Builtins/Etc/ClearBuffer.cs
@@ -1,5 +1,8 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using Console.Errors;
+using Console.Extensions;
 
 namespace Console.Commands.Builtins.Etc;
 
@@ -19,5 +22,53 @@ public class ClearBufferCommand : BaseBuiltinCommand
 
     public override string DocString => $@"
 This command will clear the Terminal buffer.
+";
+}
+
+public class ElevateCommand : BaseBuiltinCommand
+{
+    public override string Name => "elevate";
+    public override string Description => "Elevate the command prompt to administrator. (requires restart)";
+
+    public override CommandResult Run(List<string> args, IConsole parent)
+    {
+        base.Run(args, parent);
+
+        var ourPath = parent.GetExecutableLocation();
+        var path = Path.Combine(ourPath, "Elevate.exe");
+
+        if (!File.Exists(path))
+        {
+            WriteLine("This build does not appear to have `Elevate.exe`.");
+            WriteLine("We expect the root project path to contain `Elevate.exe` so we can elevate.");
+            return 0;
+        }
+
+        var ourProcessId = Environment.ProcessId;
+        var ourProcessPath = Path.Combine(parent.GetExecutableLocation(), "Console.exe");
+
+        var startInfo = new ProcessStartInfo
+        {
+            FileName = path,
+            Arguments = $"-- {ourProcessPath} {ourProcessId}"
+        };
+
+        try
+        {
+            var proc = Process.Start(startInfo);
+            proc?.WaitForExit();
+        }
+        catch (Exception ex)
+        {
+            WriteLine("failed to elevate: " + ex.ToString());
+        }
+
+        return 0;
+    }
+
+    public override string DocString => $@"
+Elevate the current running process to admin.
+
+This does not occur if the process is already administrator.
 ";
 }
